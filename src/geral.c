88e64857a31f40c3e24b95039316a9fb90a20932
Enzo Varasquim <enzovarasribeiro@gmail.com>
2026-09-05 20:06:19 -0300

    Adicionado: ajustes na função de reset para as posições dos inimigos sempre serem aleatórias

--- src/geral.c
+++ src/geral.c
@@ -1,12 +1,14 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include "geral.h"
+#include "inimigos.h"
 
 #define MAX_TIROS 30
 
 //tela
 int width = 640;
 int height = 480;
+
 
 void desenhar_background(void) {
     al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -19,9 +21,14 @@ void desenhar_background(void) {
     }
 }
 
-void reset(Player* player, Inimigo* inimigo, Tiro tiros[]) {
+void reset(Player* player, Inimigo qtd_inimigos[], int total_inimigos, Tiro tiros[]) {
     *player = init_player();
-    *inimigo = init_inimigo();
+
+    for (int i = 0; i < total_inimigos; i++) {
+        int posX = qtd_inimigos[i].size + (rand() % (width - 2 * qtd_inimigos[i].size + 1));
+        int posY = qtd_inimigos[i].size + (rand() % (height - 2 * qtd_inimigos[i].size + 1));
+        qtd_inimigos[i] = init_inimigo(posX, posY);
+    }
 
     for (int i = 0; i < MAX_TIROS; i++) {
         tiros[i] = init_tiro();
